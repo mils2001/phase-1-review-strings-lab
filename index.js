3f62ff24a2ad49fed06f5d14d577adfbe4485850
chris <Mileschris466@gmail.com>
2024-09-27 14:21:03 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+
+let currentUser = 'Miles';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+
+
+
+const shortGreeting = ` Welcome, M!, ${currentUser[0]}!`
+
+
+
+
+
+
+
+
+
+
+
+
